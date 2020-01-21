1f6373ac987ef9d08b9ae70c43763cdb327280c5
Neelkamal <neelkamal.17u131@viit.ac.in>
2020-01-21 22:39:57 +0530

    working code

--- Trees/MirrorImage.cpp
+++ Trees/MirrorImage.cpp
@@ -10,7 +10,7 @@
     ::::::::    VIIT, PUNE    ::::::::
  */
 
-
+//https://www.hackerearth.com/practice/data-structures/trees/binary-and-nary-trees/practice-problems/algorithm/mirror-image-2/description/
 
 #include<bits/stdc++.h>
 #define llint long long int
@@ -50,17 +50,44 @@ int main() {
     fastio
     int n, q, a, b;
     char c;
+    int i1, i2;
 
     
 
     cin >> n >> q;
-    vi map(2*n+1), tree(2*n+1), mmap(2*n+1), mtree(2*n+1);
+    vi map(10000, -1), tree(10000, -1), mmap(10000, -1), mtree(10000, -1);
+    map[1] = 1;
+    mmap[1] = 1;
+    tree[1] = 1;
+    mtree[1] = 1;
     
     for (int i = 0; i < n-1; i++) {
         cin >> a >> b >> c;
         if (c == 'R') {
+            i1 = map[a]*2 + 1;
+            i2 = mmap[a]*2;
             
         }
+        else {
+            i1 = map[a]*2;
+            i2 = mmap[a]*2 + 1;
+        }
+
+        map[b] = i1;
+        mmap[b] = i2;
+        tree[i1] = b;
+        mtree[i2] = b;
+        
+        
+    }
+    
+    //for (int i = 0; i < n+5; i++) cout << tree[i]; cout <<endl;
+    //for (int i = 0; i < n+5; i++) cout << mtree[i]; cout <<endl;
+    
+
+    for (int i = 0; i < q; i++) {
+        cin >> a;
+        cout << mtree[map[a]] << '\n';
     }
 
     
